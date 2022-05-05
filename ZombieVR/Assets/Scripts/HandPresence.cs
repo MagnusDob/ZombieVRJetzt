2f113e3efd9f2bb75ac3a4c707ffcfff2d086091
Magnus Dobner <dobner.magnus@stud.hs-fresenius.de>
2022-05-05 13:23:14 +0200

    Test

---
+++ ZombieVR/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+                }
+    }
+
+    void Update()
+    {
+        
+    }
+}
